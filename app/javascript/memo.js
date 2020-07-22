a7be97de12f13764c6463fc5531b9df9a33d4082
takeru89 <t.sada1989@gmail.com>
2020-07-22 14:55:44 +0900

    修正1

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,4 +1,4 @@
-function() {
+function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     const formData = new FormData(document.getElementById("form"));
